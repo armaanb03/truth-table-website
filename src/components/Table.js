c23a35b40ebed7e52260e3eb872ff6fc395b5058
Armaan <armaan.b03@gmail.com>
2023-02-21 22:25:59 -0500

    Overhaul all visuals and theme of webpage

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,27 +4,43 @@ import './Table.css'
 const DynamicTable = ({ data, message }) => {
   const headers = Object.keys(data[0].assignments);
 
+  const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+      x= e.clientX - rect.left,
+      y= e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`)
+    target.style.setProperty("--mouse-y", `${y}px`)
+  }
+
   return (
-    <table class="truth-table">
-      <thead class="header">
-        <tr>
-          {headers.map((header, index) => (
-            <th key={index}>{header}</th>
-          ))}
-          <th>{message.replaceAll('(', '').replaceAll(')', '')}</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            {headers.map((header, index) => (
-              <td key={index}>{item.assignments[header]}</td>
+    <div class="table-wrapper" onMouseMove={handleOnMouseMove}>
+      <div class="table-border"></div>
+      <div class="table-content">
+        <table class="truth-table">
+          <thead class="header">
+            <tr>
+              {headers.map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+              <th>{message.replaceAll('(', '').replaceAll(')', '')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                {headers.map((header, index) => (
+                  <td key={index}>{item.assignments[header]}</td>
+                ))}
+                <td>{item.value}</td>
+              </tr>
             ))}
-            <td>{item.value}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
